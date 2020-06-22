c5cd21a86026e81777674fd8e0a067fb5e8e5554
Willian de Mattos <willian_matt@hotmail.com>
2020-06-22 10:36:33 -0300

    #1250 Link de ativação de clientes por SMS
    http://workplace.procedo.com.br/tasks/form/==gMBpmT

---
+++ src/SendMultiple.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace ApiSendSms;
+
+use Exception;
+use stdClass;
+
+class SendMultiple
+{
+    private $ret;
+
+    public function __construct()
+    {
+        if (!isset(ENVIRONMENT)) {
+            define('ENVIRONMENT', 'production');
+        }
+
+        $this->ret = new stdClass();
+        $this->ret->status = false;
+        $this->ret->mensagem = '';
+    }
+
+    public function sendSms($body)
+    {
+        try {
+            $this->body = $this->_defineBody($body);
+
+            $result = $this->_send();
+            // Converte resposta em json
+            $result = json_decode($result, true);
+
+            if ($result->status == FALSE) {
+                throw new Exception($result->mensagem);
+            }
+
+            $this->ret->status = true;
+            $this->ret->mensagem = $result->mensagem;
+            $this->ret->logs = $result->logs;
+        } catch (Exception $e) {
+            $this->ret->status = false;
+            $this->ret->mensagem = $e->getMessage();
+        }
+
+        return $this->ret;
+    }
+
+
+    private function _defineBody($body)
+    {
+        try {
+            $result = [
+                'sms' => ''
+            ];
+
+            if (!empty($body) && is_array($body)) {
+                foreach ($body as $b) {
+                    if (empty($b['celular']) && empty($b['mensagem']))
+                        continue;
+
+                    $result['sms'][] = [
+                        'to' => $b['celular'],
+                        'msg' => $b['mensagem']
+                    ];
+                }
+            }
+
+            if(empty($result))
+                throw new Exception('Array de dados Vazio');
+
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+
+        return $result;
+    }
+
+    private function _send()
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->_getApiHost());
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->body);
+
+        return curl_exec($ch);
+    }
+
+    private function _getApiHost()
+    {
+        $url = '';
+        switch (ENVIRONMENT) {
+            case 'development':
+                $url = 'http://localhost:3100/send';
+                break;
+            case 'production':
+                $url = 'http://api-sms.procedo.com.br/send';
+                break;
+        }
+        return $url;
+    }
+}
